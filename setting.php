6c26e87c4db49827e643a369d04246721368a485
sexyonly <102304540+sexyonly@users.noreply.github.com>
2022-03-29 00:42:11 +0700

    Delete setting.php

--- setting.php
+++
@@ -1,8 +0,0 @@
-<?php
-// setting scama
-// License Auth By SCHT
-// Jasa Bypass Bot PM sini BAWA UANG JANGAN LUPA WKKWKW
-$botToken="5101088519:AAFuLcd-Z45YDC5xbTvPLqfBJo13gGf5GMw"; // token bot telegram
-$chatId="894561405";  // chatId telegram
-
-?>
